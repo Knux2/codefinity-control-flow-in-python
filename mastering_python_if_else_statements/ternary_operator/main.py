23e4bc95230536c5ef1e937f9da52c58ee306f11
Knux2 <>
2026-04-16 21:21:41 +0000

    Ternary Operator

--- mastering_python_if_else_statements/ternary_operator/main.py
+++ mastering_python_if_else_statements/ternary_operator/main.py
@@ -3,7 +3,7 @@ true_message = "You've met your hydration goal!"
 false_message = "Drink more water to reach your goal."
 
 # Using ternary operator to check hydration goal
-message = ___
+message = true_message if water_intake >= 2 else false_message
 
 # Testing
 print("Hydration Status:", message)
